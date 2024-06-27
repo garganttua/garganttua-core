dda93bf2cebc40ef92690dcc497189b20a7deb49
Colombet Jérémy <gitlab@citech.fr>
2024-06-27 16:16:18 +0200

    fds

--- garganttua-reflection/src/main/java/com/garganttua/reflection/beans/GGBeanSupplier.java
+++ garganttua-reflection/src/main/java/com/garganttua/reflection/beans/GGBeanSupplier.java
@@ -1,13 +1,9 @@
 package com.garganttua.reflection.beans;
 
-import java.io.File;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,7 +21,7 @@ public class GGBeanSupplier implements IGGBeanSupplier {
 	public GGBeanSupplier(Collection<String> packages) {
 		packages.parallelStream().forEach(package_ -> {
 			try {
-				List<Class<?>> annotatedClasses = this.getClassesWithAnnotation(package_, GGBean.class);
+				List<Class<?>> annotatedClasses = GGObjectReflectionHelper.getClassesWithAnnotation(package_, GGBean.class);
 				annotatedClasses.forEach( annotatedClass -> {
 					GGBeanFactory beanFactory = new GGBeanFactory(annotatedClass.getAnnotation(GGBean.class), annotatedClass);
 					this.beans.add(beanFactory);
@@ -40,48 +36,6 @@ public class GGBeanSupplier implements IGGBeanSupplier {
 	public String getName() {
 		return BEAN_SUPPLIER;
 	}
-	
-	private List<Class<?>> getClassesWithAnnotation(String packageName, Class<? extends Annotation> annotation) throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        List<Class<?>> classes = new ArrayList<>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        List<Class<?>> annotatedClasses = new ArrayList<>();
-        for (Class<?> clazz : classes) {
-            if (clazz.isAnnotationPresent(annotation)) {
-                annotatedClasses.add(clazz);
-            }
-        }
-        return annotatedClasses;
-    }
-
-    private List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class<?>> classes = new ArrayList<>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    assert !file.getName().contains(".");
-                    classes.addAll(findClasses(file, packageName + "." + file.getName()));
-                } else if (file.getName().endsWith(".class")) {
-                    classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-                }
-            }
-        }
-        return classes;
-    }
 
     @Override
 	public Object getBeanNamed(String name) throws GGReflectionException {
--- garganttua-reflection/src/main/java/com/garganttua/reflection/utils/GGObjectReflectionHelper.java
+++ garganttua-reflection/src/main/java/com/garganttua/reflection/utils/GGObjectReflectionHelper.java
@@ -1,11 +1,16 @@
 package com.garganttua.reflection.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -139,9 +144,50 @@ public class GGObjectReflectionHelper {
 	
 	public static boolean isImplementingInterface(Class<?> interfaceType, Class<?> objectType) {
 		List<Class<?>> interfaces = Arrays.asList(objectType.getInterfaces());
-		
 		Optional<Class<?>> found = interfaces.parallelStream().filter(c -> c.equals(interfaceType)).findFirst();
 		return found.isPresent();
 	}
+	
+	public static List<Class<?>> getClassesWithAnnotation(String packageName, Class<? extends Annotation> annotation) throws ClassNotFoundException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        assert classLoader != null;
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList<>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        List<Class<?>> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        List<Class<?>> annotatedClasses = new ArrayList<>();
+        for (Class<?> clazz : classes) {
+            if (clazz.isAnnotationPresent(annotation)) {
+                annotatedClasses.add(clazz);
+            }
+        }
+        return annotatedClasses;
+    }
+
+    private static List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
+        List<Class<?>> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    assert !file.getName().contains(".");
+                    classes.addAll(findClasses(file, packageName + "." + file.getName()));
+                } else if (file.getName().endsWith(".class")) {
+                    classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+                }
+            }
+        }
+        return classes;
+    }
 
 }
