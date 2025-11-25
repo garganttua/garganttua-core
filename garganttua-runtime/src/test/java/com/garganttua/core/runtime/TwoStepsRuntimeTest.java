ada15bb11216467e65b835e36c9f5a74edf0330d
Jérémy Colombet <colombetj@gmail.com>
2025-11-25 13:34:26 +0100

    runtime with two steps tests

--- garganttua-runtime/src/test/java/com/garganttua/core/runtime/TwoStepsRuntimeTest.java
+++ garganttua-runtime/src/test/java/com/garganttua/core/runtime/TwoStepsRuntimeTest.java
@@ -56,24 +56,68 @@ public class TwoStepsRuntimeTest {
     }
 
     @Test
-    public void testNominal(){
+    public void testNominal() {
 
-        IRuntime<String,String> runtime = get(builder().runtime("two-steps-runtime", String.class, String.class).variable("step-one-variable","step-one-variable").variable("output-step-variable","output-step-variable").up());
+        IRuntime<String, String> runtime = get(builder().runtime("two-steps-runtime", String.class, String.class)
+                .variable("step-one-variable", "step-one-variable")
+                .variable("output-step-variable", "output-step-variable").up());
 
-        IRuntimeResult<String,String> result = runtime.execute("test").orElseThrow();
+        IRuntimeResult<String, String> result = runtime.execute("test").orElseThrow();
 
         assertEquals(222, result.code());
-        assertEquals("test-step-one-processed-step-one-variable-output-step-processed-output-step-variable", result.output());
+        assertEquals("test-step-one-processed-step-one-variable-output-step-processed-output-step-variable",
+                result.output());
     }
 
     @Test
-    public void testCatchedExceptionInOutputStep(){
+    public void testCatchedExceptionInOutputStep() {
 
-        IRuntime<String,String> runtime = get(builder().runtime("two-steps-runtime", String.class, String.class).variable("step-one-variable","step-one-variable").variable("output-step-variable","di-exception").up());
+        IRuntime<String, String> runtime = get(builder().runtime("two-steps-runtime", String.class, String.class)
+                .variable("step-one-variable", "step-one-variable").variable("output-step-variable", "di-exception")
+                .up());
 
-        IRuntimeResult<String,String> result = runtime.execute("test").orElseThrow();
+        IRuntimeResult<String, String> result = runtime.execute("test").orElseThrow();
 
         assertEquals(444, result.code());
         assertEquals("test-step-one-processed-step-one-variable-output-step-fallback", result.output());
+    }
+
+    @Test
+    public void testCatchedExceptionInStepOneStep() {
+
+        IRuntime<String, String> runtime = get(builder().runtime("two-steps-runtime", String.class, String.class)
+                .variable("step-one-variable", "di-excpetion").variable("output-step-variable", "di-exception").up());
+
+        IRuntimeResult<String, String> result = runtime.execute("test").orElseThrow();
+
+        assertEquals(444, result.code());
+        assertEquals("test-step-one-processed-di-excpetion-output-step-fallback", result.output());
+    }
+
+    @Test
+    public void testUncatchedExceptionInStepOneStep_abortOnUncatchedException_false() {
+
+        IRuntime<String, String> runtime = get(builder().runtime("two-steps-runtime", String.class, String.class)
+                .variable("step-one-variable", "custom-excpetion")
+                .variable("output-step-variable", "output-step-variable").up());
+
+        IRuntimeResult<String, String> result = runtime.execute("test").orElseThrow();
+
+        assertEquals(222, result.code());
+        assertEquals("test-step-one-processed-custom-excpetion-output-step-processed-output-step-variable",
+                result.output());
+    }
+
+    @Test
+    public void testUncatchedExceptionInOutputStep_abortOnUncatchedException_true() {
+
+        IRuntime<String, String> runtime = get(builder().runtime("two-steps-runtime", String.class, String.class)
+                .variable("step-one-variable", "step-one-variable").variable("output-step-variable", "custom-exception")
+                .up());
+
+        IRuntimeResult<String, String> result = runtime.execute("test").orElseThrow();
+
+        assertEquals(50, result.code());
+        assertEquals(null, result.output());
     }
 }
